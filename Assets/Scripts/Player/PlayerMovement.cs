e6faea2f4b4614a12c86e804386ca8152dbe319e
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-05-02 01:07:25 +0200

    Rig Animation and Movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,23 +7,34 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        Vector2 targetVelocity = moveInput * moveSpeed;
-        if (rb.linearVelocity != targetVelocity)
-        {
-            rb.linearVelocity = targetVelocity;
-        }
+        rb.linearVelocity = moveInput * moveSpeed;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
+        // If the movement input is performed, start the walking animation
+        animator.SetBool("isWalking", true);
+
+        // If the movement input is canceled, stop the walking animation and set the last input direction
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        // Read the movement input
         moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
