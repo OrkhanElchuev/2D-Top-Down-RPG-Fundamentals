b04761aabb94b9fbd02d3b1e10cbf435ab10c1ff
OrkhanElchuev <orkhan.elchuev@gmail.com>
2026-05-04 15:04:02 +0200

    Setup the Save Data

--- Assets/Scripts/Save System/SaveManager.cs
+++ Assets/Scripts/Save System/SaveManager.cs
@@ -1,17 +1,24 @@
+using System.IO;
+using Cinemachine;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    
+    private string saveLocation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Set the save location to a file named "saveData.json" in the persistent data path
+        saveLocation = Path.Combine(Application.persistentDataPath, "saveData.json");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGame(SaveData data)
     {
-        
+        SaveData saveData = new SaveData
+        {
+            playerPos = GameObject.FindGameObjectWithTag("Player").transform.position,
+            mapBoundry = FindAnyObjectByType<CinemachineConfiner>().m_BoundingShape2D.gameObject.name
+        };
     }
 }
